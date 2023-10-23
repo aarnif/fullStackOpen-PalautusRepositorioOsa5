620a5dcb23e61f642c39af52704f50b93e84eea7
aarnif <aarni.fagerstrom@gmail.com>
2023-10-23 09:45:41 +0300

    Tehtava 5.7

--- frontend/src/components/Blog.jsx
+++ frontend/src/components/Blog.jsx
@@ -1,7 +1,42 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
+import "../index.css";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid 1px",
+    marginBottom: 5,
+  };
+
+  const showBlogInfo = () => {
+    if (showDetails) {
+      return (
+        <>
+          <div>{blog.url}</div>
+          <div>
+            like {blog.likes} <button>like</button>
+          </div>
+          <div>{blog.author}</div>
+        </>
+      );
+    }
+  };
+
+  return (
+    <div style={blogStyle}>
+      {" "}
+      <div>
+        {blog.title} {blog.author}{" "}
+        <button onClick={() => setShowDetails(!showDetails)}>
+          {showDetails ? "hide" : "view"}
+        </button>
+        {showBlogInfo()}
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
