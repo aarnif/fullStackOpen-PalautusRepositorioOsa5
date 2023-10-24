ed59e578a01881e7b507ec21e1e6b8686ec6644a
aarnif <aarni.fagerstrom@gmail.com>
2023-10-24 10:59:00 +0300

    Refaktoroi edellisen tehtavan testia ja tee tehtava 5.14

--- frontend/src/components/Blog.test.js
+++ frontend/src/components/Blog.test.js
@@ -1,14 +1,23 @@
 import React from "react";
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Blog from "./Blog";
 
-test("renders blog title", () => {
-  const blog = {
-    title: "React patterns",
-    id: "5a422a851b54a676234d17f7",
-  };
+const blog = {
+  title: "React patterns",
+  author: "Michael Chan",
+  user: {
+    name: "John Doe",
+    username: "johnD",
+    id: "652fe14972b0816f712d92c2",
+  },
+  url: "https://reactpatterns.com/",
+  likes: 1023,
+  id: "5a422a851b54a676234d17f7",
+};
 
+test("renders blog title", () => {
   render(
     <Blog
       key={blog.id}
@@ -19,7 +28,27 @@ test("renders blog title", () => {
     />
   );
 
-  const element = screen.getByText(blog.title);
+  screen.getByText(/React patterns/i);
+});
 
-  expect(element).toBeDefined();
+test("renders blog title, author, url, likes and user after view button is pressed", async () => {
+  render(
+    <Blog
+      key={blog.id}
+      blog={blog}
+      userName={""}
+      updateBlog={() => {}}
+      deleteBlog={() => {}}
+    />
+  );
+
+  const user = userEvent.setup();
+  const button = screen.getByText("view");
+  await user.click(button);
+
+  screen.getByText(/React patterns/i);
+  screen.getByText(/Michael Chan/i);
+  screen.getByText(/https:\/\/reactpatterns.com\//i);
+  screen.getByText(/1023/i);
+  screen.getByText(/John Doe/i);
 });
