5d0ac3972d5c4595b2f81b1cd0abbccdff3bcf2f
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2026-06-20 16:52:39 +0300

    Lataa backend URL ympäristömuuttujasta

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,20 @@
-import { defineConfig } from "vite";
+/* eslint-disable no-undef */
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3003",
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_BACKEND_URL,
+          changeOrigin: true,
+        },
       },
     },
-  },
+  };
 });
